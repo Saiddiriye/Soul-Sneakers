b4b2bdf9be891ec0577b72d855570d433ac79416
Saiddiriye <143405830+Saiddiriye@users.noreply.github.com>
2023-10-22 14:56:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const toggleButton = document.querySelector('.toggle-button');
+const navbarWrapper = document.querySelector('.navbar-wrapper');
+
+toggleButton.addEventListener('click', function () {
+    navbarWrapper.classList.toggle('nav-mobile-show');
+});
 
